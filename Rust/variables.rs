4effa4d180c237c7b973c727d06f86f737b31dd7
Sudhamsh Reddy <sudhamshreddy07@gmail.com>
2024-08-08 19:12:55 -0400

    basic codes

---
+++ Rust/variables.rs
@@ -0,0 +1,26 @@
+// variables are similar to c with the exception of 128 bit signed and unsigned int 
+// arch can be used to inherit system instruction size
+// fn main(){
+//     let name = "Sudhamsh";
+//     let age = 24;
+//     let height = 5.8;
+//     println!("Name :{}\t age:{} \nheight:{}",name,age,height);
+// }
+
+
+
+//throws error
+// fn main() {
+//     let interest:f32 = 8;   // integer assigned to float variable
+//     println!("interest is {}",interest);
+//  }
+
+
+//separator for easy reading
+
+// fn main(){
+//     let a = 123_835.13_001;
+//     println!("number :{}",a);
+// }
+
+// char
